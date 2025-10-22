a4f2bb36ada70c24421c14402d9f82c9d5aba443
Tabitha Becker <tabithabecker81@gmail.com>
2025-10-22 12:42:01 -0500

    Update hw2Script.js

--- hw2Script.js
+++ hw2Script.js
@@ -1,10 +1,23 @@
-function removedata1() {
-  document.getElementById("").innerHTML = "";
-}
-
 // Adding in script to bring in current date. Found on W3schools.com
-function getDate() {
-  const d = new Date();
-	let currentDay = d.toLocaleDateString();
-	document.getElementById ("today").innerHTML = currentDay; 
+document.addEventListener('DOMContentLoaded', (event) => {
+  function getDate() {
+    const dateChange = new Date();
+	  let currentDay = dateChange.toLocaleDateString();
+	  document.getElementById ("today").innerHTML = currentDay; 
+  }
+  getDate();
+})
+
+/*<!--Adding script to bring footer into form. Script found at 
+https://stackoverflow.com/questions/63663201/i-use-a-fetch-statement-to-retrieve-my-html-footer-and-include-it-on-every-page*/
+function getFooter() {
+  fetch('hw2-footer.html') //Fetching the footer
+    .then(response => response.text())
+    .then(data => {
+      const footer = document.getElementById('footerPlaceholder');
+      if (footerPlacholder) {
+      footerPlaceholder.innerHTML = data;
+      }
+    })
+    .catch(error => console.error('Error loading footer:', error));
 }
